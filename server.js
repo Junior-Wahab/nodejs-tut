99de3ba0a6fca0958fa09ae7c947b37274fa5fd0
jnrchry <jnrchry@gmail.com>
2024-10-14 16:57:17 +0000

    Task

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 import http from "http";
+const PORT = process.env.PORT;
 
-const PORT = 8080;
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
+  // create a router
+  try {
+    // check if GET request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        // res.writeHead(500, { "Content-Type": "application/json" });
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end(`<h1> Hello </h1>`);
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(`<h1> About </h1>`);
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end(`<h1> Not found </h1>`);
+      }
+    } else {
+      throw new Error(`Method not allowed`);
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end(`<h1> Server error </h1>`);
+  }
+
+  //   res.setHeader("Content-Type", "text/html");
+
+  // request
+  //   console.log(req.url);
+  //   console.log(req.method);
 
   // change status
-  res.statusCode = 404;
+  //   res.statusCode = 404;
 
-  res.writeHead(500, { "Content-Type": "application/json" });
-
-  res.write("Hello World");
-  res.end(`<h1> Hello </h1>`);
+  //   res.write("Hello World");
 });
 
 server.listen(PORT, () => {
